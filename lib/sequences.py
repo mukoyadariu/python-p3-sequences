646f25961d433e9677fbe7676916ae7f312d6d24
mukoyadariu <mukoya.darius@student.moringaschool.com>
2023-08-15 15:12:08 +0300

    completed lab assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fib_sequence = [0, 1]
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    while len(fib_sequence) < length:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    print(fib_sequence)
+
+# Example usage
+print_fibonacci(10)
